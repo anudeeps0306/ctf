078be6cce31f11c8353d640ad3d73ba21b48556a
KPR111 <105378982+KPR111@users.noreply.github.com>
2023-04-08 12:57:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Login from './components/Login/Login'
-import Signup from './components/Signup/Signup'
+// export default App
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='main'>
-        <Navbar/>
-        <Login />
+    <div className="main">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
