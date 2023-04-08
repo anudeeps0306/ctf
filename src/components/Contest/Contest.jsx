fca39ddfe37c4960e5a1db6c6f92d54bc2eec56a
KPR111 <105378982+KPR111@users.noreply.github.com>
2023-04-08 15:59:44 +0530

    updated links

--- src/components/Contest/Contest.jsx
+++ src/components/Contest/Contest.jsx
@@ -1,46 +1,47 @@
 import React from "react";
 import "./contest.css";
 import Card from "../Card/Card";
+import { Link } from "react-router-dom";
 
 const data = [
-    {
-      contest: {
-        name: "Contest Name 1",
-        author: "Author 1",
-        when: "2023-04-10T14:30:00.000Z",
-        start_time: "16:30:00",
-        url: "https://www.google.com"
-      }
+  {
+    contest: {
+      name: "Contest Name 1",
+      author: "Author 1",
+      when: "2023-04-10T14:30:00.000Z",
+      start_time: "16:30:00",
+      url: "https://www.google.com",
     },
-    {
-      contest: {
-        name: "Contest Name 2",
-        author: "Author 2",
-        when: "2023-04-15T09:00:00.000Z",
-        start_time: "12:00:00",
-        url: "https://www.yahoo.com"
-      }
+  },
+  {
+    contest: {
+      name: "Contest Name 2",
+      author: "Author 2",
+      when: "2023-04-15T09:00:00.000Z",
+      start_time: "12:00:00",
+      url: "https://www.yahoo.com",
     },
-    {
-      contest: {
-        name: "Contest Name 3",
-        author: "Author 3",
-        when: "2023-04-20T18:00:00.000Z",
-        start_time: "20:00:00",
-        url: "https://www.bing.com"
-      }
+  },
+  {
+    contest: {
+      name: "Contest Name 3",
+      author: "Author 3",
+      when: "2023-04-20T18:00:00.000Z",
+      start_time: "20:00:00",
+      url: "https://www.bing.com",
     },
-    {
-      contest: {
-        name: "Contest Name 4",
-        author: "Author 4",
-        when: "2023-04-25T07:30:00.000Z",
-        start_time: "10:30:00",
-        url: "https://www.duckduckgo.com"
-      }
-    }
-  ];
-  
+  },
+  {
+    contest: {
+      name: "Contest Name 4",
+      author: "Author 4",
+      when: "2023-04-25T07:30:00.000Z",
+      start_time: "10:30:00",
+      url: "https://www.duckduckgo.com",
+    },
+  },
+];
+
 const Contest = () => {
   return (
     <div className="contest-main">
@@ -62,13 +63,15 @@ const Contest = () => {
         <div className="contest-main-contest">
           {data.map((contest, index) => (
             <div className="contest-main-card" key={index}>
-              <Card
-                name={contest.contest.name}
-                author={contest.contest.author} // pass author instead of description
-                when={contest.contest.when}
-                startTime={contest.contest.start_time}
-                url={contest.contest.url}
-              />
+              <Link to="/contest_page" style={{textDecoration:"none"}}>
+                <Card
+                  name={contest.contest.name}
+                  author={contest.contest.author} // pass author instead of description
+                  when={contest.contest.when}
+                  startTime={contest.contest.start_time}
+                  url={contest.contest.url}
+                />
+              </Link>
             </div>
           ))}
         </div>
